7f2affba358dde762593be9db823bedec8f67e51
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-11 14:48:24 +0700

    prepare

---
+++ GravityRunner/Scenes/FinishGame/FinishGame.swift
@@ -0,0 +1,13 @@
+//
+//  FinishGame.swift
+//  GravityRunner
+//
+//  Created by punyawee  on 11/8/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import SpriteKit
+
+class FinishGame: SKScene {
+
+}
---
+++ GravityRunner/Scenes/MainMenu/MainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenu.swift
+//  GravityRunner
+//
+//  Created by punyawee  on 11/8/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+
+}
